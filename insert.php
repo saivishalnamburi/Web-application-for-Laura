1a1e2efc8862775dbc2e8d0e6fd7e01d0174391b
dsouzalenroy <54295102+dsouzalenroy@users.noreply.github.com>
2020-01-23 09:36:19 +1000

    Add files via upload

--- insert.php
+++ insert.php
@@ -1,96 +1,64 @@
-<?php
 
-$con = mysqli_connect("localhost","ictatjcu_subject","123zxc","ictatjcu_subject");
-
-$sql = "SELECT * FROM groups INNER JOIN student_details ON groups.pname = student_details.pname";
-$result = mysqli_query($connect, $sql);
-if($con)
-{
-	
-	$file = $_FILES['csvfile']['tmp_name'];
-	$handle = fopen($file,"r");
-	$i = 0;
-	
-function randStrGen($len)
-{
-    $result = "";
-    $chars = "abcdefghijklmnopqrstuvwxyz0123456789";
-    $charArray = str_split($chars);
-    for($i = 0; $i < $len; $i++){
-	    $randItem = array_rand($charArray);
-	    $result .= "".$charArray[$randItem];
-    }
-    return $result;
-}
-
-
-	
-	$groups = array();
-	while(($cont = fgetcsv($handle,1000,",")) !== false) 
-	{
-		if (!in_array($cont[9], $groups))
-		{
-			array_push($groups, $cont[9]);
-		
-			$query_groups = "INSERT INTO groups
-			(availyear, speriod, teamno, pname) 
-			VALUES('$cont[5]', '$cont[6]','$cont[8]','$cont[9]')";
-			echo $query_groups, "<br>";
-			mysqli_query($con,$query_groups);
-		
-		}	
-		
-		
-		$new_token = randStrGen(12);
-		$query_students = "INSERT INTO `student_details`(`snum`, `lname`, `fname`, `email`, `scode`, `login`, `pname`,`token`) 
-		VALUES('$cont[0]', '$cont[1]','$cont[2]','$cont[3]','$cont[4]','$cont[7]','$cont[9]','$new_token')";
-		echo $query_students, "<br>";
-		mysqli_query($con,$query_students);
-		
-		$i++;
-	}
-	
-
-}
-else
-{
-	echo"connection failed";
-}
-?>
-
-<!doctype html>
+<!DOCTYPE html>
 <html>
 <head>
-<meta charset="utf-8">
-<title>Student Data</title> 
+<title>PHP insertion</title>
+<link href="insert.css" rel="stylesheet">
 </head>
-
 <body>
-	<div class="container" style="width:500px;">
-	<h3 align="">
-	<div class="tabel-responsive">
-		<table class="table table-striped">
-			<tr>
-				<th>groups</th>
-				<th>student_details</th>
-				
-			</tr>
-			<?php
-			if(mysqli_num_rows($result)>0)
-			{
-				while($row = mysqli_fetch_array($result))
-				{
-					?>
-					<tr>
-						<td> <?php echo $row["groups"]; ?> </td>
-						<td> <?php echo $row["student_details"]; ?> </td>
-					</tr>
-						?>php
-				}
-			}
-			?>
-		</table>
-		</div>
-		</div>
+<div class="maindiv">
+<!--HTML Form -->
+<div class="form_div">
+<div class="title">
+
+</div>
+<form action="insert.php" method="post">
+<!-- Method can be set as POST for hiding values in URL-->
+<h2>Client Details</h2>
+
+<label>Subject Code</label>
+<input class="input" name="subjectCode" type="text" value="">
+
+<label>Team Name:</label>
+<input class="input" name="teamName" type="text" value="">
+
+
+<label>Client Name:</label>
+<input class="input" name="clientName" type="text" value="">
+
+<label>Client Email:</label>
+<input class="input" name="clientEmail" type="text" value="">
+ 
+<label>Client Phone Number:</label>
+<input class="input" name="clientPhoneNumber" type="text" value="">
+
+<input class="submit" name="submit" type="submit" value="Insert">
+</form>
+</div>
+</div>
 </body>
 </html>
+
+
+
+<?php
+$conn = mysqli_connect("localhost", "root", "", "student"); // Establishing Connection with Server
+
+if(isset($_POST['submit'])){ // Fetching variables of the form which travels in URL
+$subjectCode = $_POST['subjectCode'];
+$teamName = $_POST['teamName'];
+$clientName = $_POST['clientName'];
+$clientEmail = $_POST['clientEmail'];
+$clientPhoneNumber = $_POST['clientPhoneNumber'];
+if($clientName !=''||$clientEmail !=''){
+//Insert Query of SQL
+$query = mysqli_query($conn,"insert into client_details(subject_code, team_name, client_name, client_email, client_phone_number ) values ('$subjectCode', '$teamName', '$clientName', '$clientEmail', '$clientPhoneNumber' )");
+echo "<br/><br/><span>Data Inserted successfully...!!</span>";
+}
+else{
+echo "<p>Insertion Failed <br/> Some Fields are Blank....!!</p>";
+}
+}
+
+?>
+
